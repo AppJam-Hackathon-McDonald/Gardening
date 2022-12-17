2a60ced2704c3b38c3bbf288ec801c9d0d68c428
chan-kim <chan.ckeem@gmail.com>
2022-12-18 00:48:35 +0900

    fix : BackEnd set

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+// routing
+const indexRouter = require("./routes/router");
+require("./db/mongoose");
+
+const PORT = process.env.PORT || 8080;
+
+// view setting
+app.use(express.static(path.join(__dirname, "./Client/build")));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/", indexRouter);
+
+// app.use("/", indexRouter);
+
+app.listen(PORT, function () {
+  console.log("listening on 8080");
+});
+
+module.exports = app;
